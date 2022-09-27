6266cdd6bf5a0b62d6803e5376dbf40e866a5101
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-27 07:01:45 +0300

    add MyHub.cs and js

--- SignalR.API/Hubs/MyHub.cs
+++ SignalR.API/Hubs/MyHub.cs
@@ -8,18 +8,40 @@ namespace SignalR.API.Hubs
 {
     public class MyHub : Hub
     {
-        public static List<string> Names { get; set; } = new List<string>();
+        private static List<string> Names { get; set; } = new List<string>();
 
+        private static int ClientCount { get; set; } = 0;
+        public static int TeamCount { get; set; } = 7;
         public async Task SendName(string name)
         {
-            Names.Add(name);
+            if (Names.Count >= TeamCount)
+            {
+                await Clients.Caller.SendAsync("Error", $"Takım en fazla {TeamCount} kişi olabilir");
+            }
+            else
+            {
+                Names.Add(name);
 
-            await Clients.All.SendAsync("ReceiveName", name);
+                await Clients.All.SendAsync("ReceiveName", name);
+            }
         }
 
         public async Task GetNames()
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+        public async override Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
