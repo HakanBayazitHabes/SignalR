2368babbd2d917d95161ab96f7eed5d8caf5c471
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-27 18:59:34 +0300

    add CovidService,CovidController,CovidHub

---
+++ CovidChart.API/Hubs/CovidHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidChart.API.Hubs
+{
+    public class CovidHub : Hub
+    {
+        public async Task GetCovidList()
+        {
+            await Clients.All.SendAsync("ReceiveCovidList", "Serviceden covid19 verilerini al");
+        }
+    }
+}
---
+++ CovidChart.API/Models/CovidService.cs
@@ -0,0 +1,36 @@
+﻿using CovidChart.API.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidChart.API.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+            await _context.Covids.AddAsync(covid);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList","data");
+        }
+
+
+
+    }
+}
