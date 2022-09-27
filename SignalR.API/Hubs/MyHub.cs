9c0e166fef35958ec9907f0ff597cf57b08e8bb9
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-27 15:43:44 +0300

    add signalR Server Hub Clients.Group

--- SignalR.API/Hubs/MyHub.cs
+++ SignalR.API/Hubs/MyHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalR.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,22 @@ namespace SignalR.API.Hubs
     public class MyHub : Hub
     {
         private static List<string> Names { get; set; } = new List<string>();
+        private AppDbContext _context;
+
+        public MyHub(AppDbContext context)
+        {
+            _context = context;
+        }
 
         private static int ClientCount { get; set; } = 0;
         public static int TeamCount { get; set; } = 7;
+
+
+        public async Task SendProduct(Product product)
+        {
+            await Clients.All.SendAsync("ReceiveProduct", product);
+        }
+
         public async Task SendName(string name)
         {
             if (Names.Count >= TeamCount)
@@ -30,6 +45,53 @@ namespace SignalR.API.Hubs
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
         }
+
+        public async Task AddToGroup(string teamName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, teamName);
+        }
+        public async Task RemoveToGroup(string teamName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, teamName);
+        }
+
+        public async Task SendNameByGroup(string Name, string teamName)
+        {
+            var team = _context.Teams.Where(x => x.Name == teamName).FirstOrDefault();
+
+            if (team != null)
+            {
+                team.Users.Add(new User { Name = Name });
+            }
+            else
+            {
+                var newTeam = new Team { Name = teamName };
+
+                newTeam.Users.Add(new User { Name = Name });
+
+                _context.Teams.Add(newTeam);
+            }
+            await _context.SaveChangesAsync();
+
+            //AddToGroup methodunda aldığı değer burda işe yarıyor. Orada Hangi grubu alıyorsa burada onu gönderiyor. Başka gruplar göremez
+            await Clients.Group(teamName).SendAsync("ReceiveMessageByGroup", Name, team.Id);
+        }
+
+        public async Task GetNamesByGroup()
+        {
+            var teams = _context.Teams.Include(x => x.Users).Select(x => new
+            {
+                teamId = x.Id,
+                Users = x.Users.ToList()
+            });
+            await Clients.All.SendAsync("ReceiveNamesByGroup", teams);
+
+        }
+
+
+
+
+
         public async override Task OnConnectedAsync()
         {
             ClientCount++;
--- SignalR.API/Startup.cs
+++ SignalR.API/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SignalR.API.Hubs;
+using SignalR.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,11 @@ namespace SignalR.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+            });
+
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy", builder =>
